153db7884176b455d3c4822d84f941d5a3f9144f
Aleksei <alex.jellydev@gmail.com>
2019-08-14 23:57:34 +0300

    generated two random hands

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import { PlayAgainButton } from './components/PlayAgainButton/PlayAgainButton';
 import { IPlayer } from './components/Player/Player';
 import { PlayerList } from './components/PlayerList/PlayerList';
 
-const SUITS = ['spade', 'heart', 'diamond', 'club'];
-const CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-
 export interface AppState {
+  allCards: string[];
+  availableCards: string[];
   players: IPlayer[];
 }
 
 export class App extends PureComponent<{}, AppState> {
 
+  static readonly SUITS = ['spade', 'heart', 'diamond', 'club'];
+  static readonly CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  static readonly CARDS_IN_HAND = 5;
+
   constructor(props: Readonly<{}>) {
     super(props);
 
+    const allCards = this.parseCards();
+
     this.state = {
+      allCards,
+      availableCards: allCards,
       players: [
         {
           name: 'Player 1',
@@ -30,6 +37,10 @@ export class App extends PureComponent<{}, AppState> {
     };
   }
 
+  componentDidMount(): void {
+    this.makeHands();
+  }
+
   render() {
     const { players } = this.state;
 
@@ -40,5 +51,59 @@ export class App extends PureComponent<{}, AppState> {
       </div>
     );
   }
+
+  private parseCards = (): AppState['allCards'] => {
+    const allCards: AppState['allCards'] = [];
+
+    App.SUITS.forEach((suit) => {
+      App.CARDS.forEach((card) => {
+        allCards.push(`${ suit }_${ card }`);
+      });
+    });
+
+    return allCards;
+  };
+
+  private makeHands = () => {
+    let availableCards = this.state.availableCards;
+
+    const playersWithHand = this.state.players.map((player) => {
+      const [hand, cardsLeft] = this.makeHand(availableCards);
+      availableCards = cardsLeft;
+
+      return {
+        ...player,
+        hand,
+      };
+    });
+
+    this.setState(() => ({
+      players: playersWithHand,
+      availableCards,
+    }));
+  };
+
+  private makeHand = (availableCards: AppState['availableCards']): [IPlayer['hand'], AppState['availableCards']] => {
+    let cardsLeft = availableCards;
+    const hand: IPlayer['hand'] = [];
+
+    for (let i = 0; i < App.CARDS_IN_HAND; i++) {
+      const [card, index] = this.getRandomCard(cardsLeft);
+      cardsLeft = cardsLeft.filter((ac, i) => i !== index);
+      hand.push(card);
+    }
+
+    return [hand, cardsLeft];
+  };
+
+  private getRandomCard = (availableCards: AppState['availableCards']): [AppState['availableCards'][0], number] => {
+    const randomIndex = this.getRandomInt(0, availableCards.length);
+
+    return [availableCards[randomIndex], randomIndex];
+  };
+
+  private getRandomInt = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
 
 }
